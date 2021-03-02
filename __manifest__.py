98a68ca26ec903ab5e34dd1ad6f18df76e9ecaee
SANTIAGO BACALE ORTIZ <you@example.com>
2021-03-02 23:09:26 +0100

    Modulo Servicios

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*- 
+
+{
+    'name': 'Servicio',
+    'summary': """Este módulo nos permitirá gestionar la asignación de servicios""",
+    'version': '0.2',
+    'description': """Este módulo nos permitirá gestionar la asignación de servicios""",
+    'author': 'Santiago Bacale Ortiz',
+    'company': 'electricistasensevilla.com',
+    'website': 'http://www.electricistasensevilla.com',
+    'category': 'Tools',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'data': [
+        'view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
